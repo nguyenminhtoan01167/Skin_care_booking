9ebead152b5c8e656ac3bf3d24ecfede4099b453
nguyenminhtoan01167 <nguyenminhtoan01167@gmail.com>
2025-04-23 17:45:41 +0700

    update

---
+++ src/main/java/ut/edu/skincarebooking/SkinCareBookingApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.skincarebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinCareBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkinCareBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ut/edu/skincarebooking/SkinCareBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.skincarebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCareBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
